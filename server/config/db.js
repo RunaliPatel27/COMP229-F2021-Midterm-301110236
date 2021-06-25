b3df843a41ff64f28c33cd702451fee023b44466
Runali Patel <rpate629@my.centennialcollege.ca>
2021-06-25 19:56:26 -0400

    Added the Mid-Term Exam Files

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+/**
+ * Add your code here to fix URI connection*/
+ module.exports = {
+
+    'URI' : 'mongodb+srv://runalipatel:WbOrIXvNUSFTLlC8@runali.3wxg2.mongodb.net/books_list?retryWrites=true&w=majority'
+};
